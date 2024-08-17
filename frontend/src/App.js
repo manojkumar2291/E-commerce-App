a8b88fccaedc4346d97f9c4d7eae3ebb3bbe9bbe
manojkumar <podapatimanoj22@gmail.com>
2024-08-17 16:41:20 +0530

    shopbycategory

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,10 @@ import Shop from './pages/Shop';
 
 import LoginSignup from './pages/LoginSignup';
 import Footer from './components/Footer/Footer'
+import men_banner from './components/assets/banner_mens.png'
+import women_banner from './components/assets/banner_women.png'
+import kids_banner from './components/assets/banner_kids.png'
+
 
 function App() {
   return (
@@ -17,9 +21,9 @@ function App() {
       <Navbar/>
       <Routes>
       <Route path='/' element={<Shop/>}/>
-      <Route path='/men' element={<Shopcategory category='men'/>} />
-      <Route path='/women' element={<Shopcategory category='women'/>} />
-      <Route path='/kids' element={<Shopcategory category='kids'/>} />
+      <Route path='/men' element={<Shopcategory banner={men_banner} category='men'/>} />
+      <Route path='/women' element={<Shopcategory banner={women_banner} category='women'/>} />
+      <Route path='/kids' element={<Shopcategory banner={kids_banner} category='kid'/>} />
       <Route path='/product' element={<Product/>}>
         <Route path=':productid' element={<Product/>}/>
       </Route>
